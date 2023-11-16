a90067c75bd06faf48f81283ad9bf0a6b4b9f6bd
Adrian6476 <adrian6476@outlook.com>
2023-11-16 02:54:59 +0000

    delete build

--- build/CMakeFiles/MainProgram.dir/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MainProgram.dir/src/main.cpp.o: \
- /home/adrian/OneDrive/Documents/Master\ of\ Computer\ Science/Professional\ Skills/respiratory/Code-Development/src/main.cpp \
- /usr/include/stdc-predef.h
--- build/external/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adrian/OneDrive/Documents/Master of Computer Science/Professional Skills/respiratory/Code-Development/external/googletest
-# Build directory: /home/adrian/OneDrive/Documents/Master of Computer Science/Professional Skills/respiratory/Code-Development/build/external/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
